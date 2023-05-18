52a72f4cf878483fde585234872ca59ae1721388
Ozéias Souza <44115785+OsouzaTI@users.noreply.github.com>
2023-05-18 08:24:09 -0400

    Update detecta_fogo.py
    Alterando detalhes

--- atividade_2/detecta_fogo.py
+++ atividade_2/detecta_fogo.py
@@ -3,20 +3,20 @@ from termcolor import colored
 
 from Constantes import FOGO_DETECTADO, AGUARDANDO
 
-class DetectcaoFogo:
+class DetectaFogo:
 
     def __init__(self) -> None:
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         self.channel = connection.channel()
-        self.channel.queue_declare(queue='temperature')
-        self.channel.queue_declare(queue='fire_alert')
+        self.channel.queue_declare(queue='temperatura')
+        self.channel.queue_declare(queue='fogo_detectado')
 
     def run(self):
 
         def callback(ch, method, properties, body):
             temperature = float(body)
             if temperature > 50:
-                self.channel.basic_publish(exchange='', routing_key='fire_alert', body=FOGO_DETECTADO)
+                self.channel.basic_publish(exchange='', routing_key='fogo_detectado', body=FOGO_DETECTADO)
                 print(colored(FOGO_DETECTADO,'red'))
             else:
                 print('Temperatura:', temperature)
@@ -26,5 +26,5 @@ class DetectcaoFogo:
         print(AGUARDANDO)
         self.channel.start_consuming()
 
-detecta = DetectcaoFogo()
+detecta = DetectaFogo()
 detecta.run()
